2946351564da03b80987c1e6a561c787bbec984a
mahardicka nurachman <mahardicka404@gmail.com>
2020-10-24 12:55:56 +0700

    mocking ws & rest api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import Axios from "axios";
+import useWebSocket from "react-use-websocket";
 
 function App() {
+  const [state, setState] = useState([]);
+  const [socket, setSocket] = useState("");
+  const { readyState, sendMessage, lastMessage } = useWebSocket(
+    "wss://echo.websocket.org"
+  );
+  const asin = useCallback(async () => {
+    const response = await Axios.get("https://reqres.in/api/users");
+    setState(response.data.data);
+  }, [setState]);
+
+  useEffect(() => {
+    asin();
+  }, [asin]);
+
+  useEffect(() => {
+    // 0 & 1
+    // send if connect
+    if (readyState) {
+      sendMessage("hello world");
+    }
+  }, [readyState, sendMessage]);
+
+  useEffect(() => {
+    // frist is null
+    if (lastMessage) {
+      setSocket(lastMessage.data);
+    }
+  }, [lastMessage]);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +49,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <p>data ws : {socket}</p>
+        <p>nama</p>
+        {state.map((item) => (
+          <p key={item.id}>{item.first_name}</p>
+        ))}
+      </main>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, act } from "@testing-library/react";
+import App from "./App";
+import Axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import ws from "jest-websocket-mock";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const whenStable = async () =>
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  });
+
+describe("test", () => {
+  const server = new ws("wss://echo.websocket.org");
+  const mock = new MockAdapter(Axios);
+
+  test("renders learn react link", async () => {
+    // mock axios rest api
+    mock.onGet("https://reqres.in/api/users").reply(200, {
+      data: [{ id: 1, first_name: "mocking from jest" }],
+    });
+    // render
+    const { container } = render(<App />);
+    // mock websocket server
+    await server.connected;
+    server.send("hello from jest");
+    // act promise
+    await whenStable();
+    // snapshot
+    expect(container).toMatchSnapshot();
+    ws.clean();
+  });
 });
